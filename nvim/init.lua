c5412aec2fb82f29d3b50cf85fb4c15d8c6d7703
Aman Oberoi <github.com.petroleum369@passfwd.com>
2025-12-25 21:56:46 +0530

    chore(nvim): move vim options to a dedicated file in config/

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,11 +2,4 @@
 require("config.lazy")
 require("config.keymaps")
 require("config.options")
-
--- bind grep to ripgrep
-vim.cmd([[set grepprg=rg\ --vimgrep\ --smart-case\ --hidden]])
-
--- relative and absolute line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%s %l %r"
+require("config.vim-options")
