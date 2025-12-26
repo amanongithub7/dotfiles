caadcdfd6e3a47b483dbc1ead6e0cfd967f8b947
Aman Oberoi <github.com.petroleum369@passfwd.com>
2025-12-26 09:07:24 +0530

    chore(nvim): vim-options file was redundant - switch back to existing lua/config/options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- bind grep to ripgrep
+vim.cmd([[set grepprg=rg\ --vimgrep\ --smart-case\ --hidden]])
+
+-- relative and absolute line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = "%s %l %r"
--- nvim/lua/config/vim-options.lua
+++
@@ -1,7 +0,0 @@
--- bind grep to ripgrep
-vim.cmd([[set grepprg=rg\ --vimgrep\ --smart-case\ --hidden]])
-
--- relative and absolute line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%s %l %r"
