f4bf2d433c2211eea9c39dc839a1683d65ec0701
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-01-24 05:24:20 +0530

    bug(nvim): markdown files with code blocks bouncing due to conceal feature bug fix with autocmd to force conceallevel to 0

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable conceal for markdown files (stops backticks in code blocks from hiding, which would lead to buffer infinite bounce glitch on scroll)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
