491179eb8bc671d54fee5bc11a22cc1db7faeeeb
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-01-25 14:58:12 +0530

    chore(nvim): remove redundant require ..autocmds,..keymaps, etc calls from init.lua which are handled by lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.autocmds")
-require("config.keymaps")
-require("config.options")
-require("config.lsp-options")
 require("config.lazy")
+require("config.lsp-options")
