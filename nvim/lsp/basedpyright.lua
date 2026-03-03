898c13085e7479d52556c27b94f06851b277f6bd
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-03-03 16:08:30 +0530

    feat(nvim): basedpyright type checking set to standard & conda env python usage

--- nvim/lsp/basedpyright.lua
+++ nvim/lsp/basedpyright.lua
@@ -2,9 +2,13 @@ vim.lsp.config("basedpyright", {
   settings = {
     basedpyright = {
       analysis = {
+        typeCheckingMode = "standard",
+        useLibraryCodeForTypes = true,
         inlayHints = {
           genericTypes = true,
         },
+        pythonPath = os.getenv("CONDA_PREFIX") and (os.getenv("CONDA_PREFIX") .. "/bin/python") or "python",
+        autoSearchPaths = true,
       },
       disableOrganizeImports = true, -- Ruff does it better
     },
