1bbe172d303f5f2b50a85db522c5a3e293e0d7a3
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-03-03 16:05:51 +0530

    chore(nvim): migration to community-maintained fork of obsidian.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -9,23 +9,23 @@ return {
     ft = { "markdown" },
   },
   { -- plugin that provides rendering, linking and other Obsidian-related functionality
-    "epwalsh/obsidian.nvim",
+    "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
     dependencies = {
-      -- Required.
+      -- Required
       "nvim-lua/plenary.nvim",
+      -- Optional
+      "saghen/blink.cmp", -- completion
+      "nvim-telescope/telescope.nvim", -- pickers
+      "nvim-treesitter/nvim-treesitter", -- syntax highlighting
+      "folke/snacks.nvim", -- snacks.image needed for image previews
     },
+    ---@module 'obsidian'
+    ---@type obsidian.config
     opts = {
+      legacy_commands = false,
+      ui = { enable = false },
       workspaces = {
         {
           name = "personal",
@@ -37,14 +37,13 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+    ft = { "markdown", "codecompanion" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
     config = function()
       require("render-markdown").setup({
         completions = { lsp = { enabled = true } },
-        preset = "obsidian",
-        enabled = false, -- render documents manually -> helps prevent conceallevel set to 2 or 3 in MD help docs (which causes visual glitches)
       })
     end,
   },
