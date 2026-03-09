62097deeabbc095732c4aed80b184a79068839b9
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-03-09 22:11:48 +0530

    chore(nvim): sync settings for vimtex

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -5,5 +5,7 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_activate = 1
   end,
 }
