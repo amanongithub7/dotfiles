b466615834bd483af7fb83f5e27813443bf57ca6
Aman Oberoi <github.com.petroleum369@passfwd.com>
2026-04-21 18:18:16 +0530

    feat(neovim): use sioyek as vimtex's pdf viewer

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -4,8 +4,27 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "skim"
-    vim.g.vimtex_view_skim_sync = 1
-    vim.g.vimtex_view_skim_activate = 1
+    --
+    -- skim as pdf viewer
+    -- vim.g.vimtex_view_method = "skim"
+    -- vim.g.vimtex_view_skim_sync = 1
+    -- vim.g.vimtex_view_skim_activate = 1
+    --
+    -- sioyek as pdf viewer
+    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_callback_progpath = "/opt/homebrew/bin/nvim"
+  end,
+  config = function()
+    vim.cmd("syntax enable")
+    vim.cmd("let g:vimtex_view_general_viewer='sioyek'")
+    -- %1: file, %2: line number
+    local options = string.format(
+      '--reuse-window --inverse-search="nvr --servername %s +%%2 %%1" --forward-search-file @tex --forward-search-line @line @pdf',
+      vim.v.servername
+    )
+
+    local command = string.format("let g:vimtex_view_general_options='%s'", options)
+    vim.cmd(command)
+    vim.cmd("let g:vimtex_compiler_progname='nvr'")
   end,
 }
