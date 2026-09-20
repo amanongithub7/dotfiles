c59091fcd9a913e5b64fb4eb6657f8968cdb6c4c
aman <github.com.petroleum369@passfwd.com>
2026-09-20 22:27:27 +0800

    chore(nvim): set opencode keybinds that dont clash with snacks picker keybinds

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -17,6 +17,19 @@ return {
     opts = {
       preferred_picker = "snacks",
       keymap = {
+        session_picker = {
+          delete_session = { "<C-x>", mode = { "i", "n" }, desc = "Delete selected sessions" },
+          new_session = { "<C-e>", mode = { "i", "n" }, desc = "Create a new session" },
+          open_in_tab = { "<C-y>", mode = { "i", "n" }, desc = "Open in new panel tab" },
+          fork_session = { "<C-o>", mode = { "i", "n" }, desc = "Fork selected session" },
+          toggle_scope = { "<C-z>", mode = { "i", "n" }, desc = "Toggle project/global scope" },
+        },
+        session_tab_picker = {
+          close_tab = { "<C-x>", mode = { "i", "n" }, desc = "Close selected panel tab" },
+        },
+        history_picker = {
+          delete_entry = { "<C-x>", mode = { "i", "n" }, desc = "Delete selected history entries" },
+        },
         editor = {
           ["<leader>om"] = { "switch_mode" },
         },
