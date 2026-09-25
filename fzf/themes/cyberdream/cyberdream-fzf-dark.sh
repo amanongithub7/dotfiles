dc04459a1a807da0bf29688183bf97e1c6304676
aman <github.com.petroleum369@passfwd.com>
2026-09-25 10:08:04 +0800

    feat(fzf): cyberdream themes for fzf

---
+++ fzf/themes/cyberdream/cyberdream-fzf-dark.sh
@@ -0,0 +1,7 @@
+# shellcheck shell=sh
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#1e2124,bg:-1,gutter:-1,spinner:#ff5ea0,hl:#ff6e5e \
+--color=fg:#ffffff,header:#ff6e5e,info:#bd5eff,pointer:#ff5ea0 \
+--color=marker:#5eff6c,fg+:#ffffff,prompt:#bd5eff,hl+:#ff6e5e \
+--color=selected-bg:#3c4048 \
+--color=border:#7b8496,label:#ffffff"
---
+++ fzf/themes/cyberdream/cyberdream-fzf-light.sh
@@ -0,0 +1,7 @@
+# shellcheck shell=sh
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#eaeaea,bg:-1,gutter:-1,spinner:#f40064,hl:#d11500 \
+--color=fg:#16181a,header:#d11500,info:#a018ff,pointer:#f40064 \
+--color=marker:#008b0c,fg+:#16181a,prompt:#a018ff,hl+:#d11500 \
+--color=selected-bg:#acacac \
+--color=border:#7b8496,label:#16181a"
